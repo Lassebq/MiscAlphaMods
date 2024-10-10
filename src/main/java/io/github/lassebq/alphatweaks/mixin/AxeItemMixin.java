dcfc2063d795ece917c63e404d5f2f78ee4661a1
Lassebq <lassebq.mine@gmail.com>
2024-10-11 01:31:35 +0300

    Add missing @Shadow. Remap AWs

--- src/main/java/io/github/lassebq/alphatweaks/mixin/AxeItemMixin.java
+++ src/main/java/io/github/lassebq/alphatweaks/mixin/AxeItemMixin.java
@@ -1,6 +1,7 @@
 package io.github.lassebq.alphatweaks.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 
 import net.minecraft.block.Block;
 import net.minecraft.item.AxeItem;
@@ -8,7 +9,7 @@ import net.minecraft.item.ToolItem;
 
 @Mixin(AxeItem.class)
 public class AxeItemMixin extends ToolItem {
-	private static Block[] EFFECTIVE_BLOCKS = new Block[]{
+	@Shadow private static Block[] EFFECTIVE_BLOCKS = new Block[]{
 		Block.PLANKS,
 		Block.BOOKSHELF,
 		Block.LOG,
